5784808f832e3d1183b8ccd698b51a221af8da57
tsufeki <tsufeki@ymail.com>
2017-01-15 23:05:48 +0100

    Detect string literal kind.

--- lib/PhpLenientParser/Expression/String_.php
+++ lib/PhpLenientParser/Expression/String_.php
@@ -11,7 +11,13 @@ class String_ extends AbstractPrefix
     {
         $token = $parser->eat();
 
+        if ($token->value[0] === 'b' || $token->value[0] === 'B') {
+            $kind = $token->value[1] === '\'' ? Scalar\String_::KIND_SINGLE_QUOTED : Scalar\String_::KIND_DOUBLE_QUOTED;
+        } else {
+            $kind = $token->value[0] === '\'' ? Scalar\String_::KIND_SINGLE_QUOTED : Scalar\String_::KIND_DOUBLE_QUOTED;
+        }
+
         $value = Scalar\String_::parse($token->value);
-        return $parser->setAttributes(new Scalar\String_($value), $token, $token);
+        return $parser->setAttributes(new Scalar\String_($value, ['kind' => $kind]), $token, $token);
     }
 }
