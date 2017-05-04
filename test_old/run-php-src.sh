1fb322239e10bfacf926d1a1375c7d75be737778
tsufeki <tsufeki@ymail.com>
2017-05-04 23:29:17 +0200

    Improve ./test_old/run-php-src.sh script slightly.

--- test_old/run-php-src.sh
+++ test_old/run-php-src.sh
@@ -1,6 +1,16 @@
-if ! [[ -a ./php-7.1.0.tar.gz ]]; then
-    wget -q https://github.com/php/php-src/archive/php-7.1.0.tar.gz
+#!/bin/bash
+
+DIR="$(dirname "$0")/.."
+ARCHIVE_URL='https://github.com/php/php-src/archive/php-7.1.0.tar.gz'
+ARCHIVE="$DIR/php-7.1.0.tar.gz"
+UNPACK_DIR="$DIR/data/php-src"
+
+if ! [[ -a "$ARCHIVE" ]]; then
+    echo 'Downloading PHP source tests...'
+    wget -q "$ARCHIVE_URL" -O "$ARCHIVE"
 fi
-mkdir -p ./data/php-src
-tar -xzf ./php-7.1.0.tar.gz -C ./data/php-src --strip-components=1
-php test_old/run.php --verbose --no-progress PHP7 ./data/php-src
+mkdir -p "$UNPACK_DIR"
+tar -xzf "$ARCHIVE" -C "$UNPACK_DIR" --strip-components=1
+echo 'Running tests...'
+php "$DIR/test_old/run.php" --verbose --no-progress PHP7 "$UNPACK_DIR"
+
